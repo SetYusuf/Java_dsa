f19df8ae3e73f372981aabc986513aace48096f4
SetYusuf <setyusuf236@gmail.com>
2026-03-15 00:01:05 +0700

    Update DynamicArray and Main.java with working code

---
+++ main.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main(String[] args) {
+        DynamicArray da = new DynamicArray(4); // initial capacity = 2
+        da.add(1);
+        da.add(2);
+        da.add(3); // automatically resizes
+        da.add(4);
+        da.add(5);
+        da.add(6);
+        da.add(7);
+        da.add(8);
+
+
+        da.print();          // Output: 1 2 3 4
+        System.out.println(da.size());       // 4
+        System.out.println(da.capacity()); // 4 or 4->8 depending on growth
+    }
+}
